c8e3607cdeb9af196f97bfbe88eae348b2d8d19a
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-14 01:43:02 +0600

    fix and update view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
-from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView, LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import CustomUser
-from .forms import CustomUserChangeForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("login")
+
+
+class CustomLoginView(LoginView):
+    template_name = "users/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("login")
+
 
 class ProfileView(LoginRequiredMixin, DetailView):
     model = CustomUser
@@ -11,6 +27,7 @@ class ProfileView(LoginRequiredMixin, DetailView):
 
     def get_object(self):
         return self.request.user
+
 
 class EditProfileView(LoginRequiredMixin, UpdateView):
     model = CustomUser
@@ -21,14 +38,17 @@ class EditProfileView(LoginRequiredMixin, UpdateView):
     def get_object(self):
         return self.request.user
 
+
 class CustomPasswordChangeView(PasswordChangeView):
     template_name = "users/change_password.html"
     success_url = reverse_lazy("profile")
+
 
 class CustomPasswordResetView(PasswordResetView):
     template_name = "users/password_reset.html"
     email_template_name = "users/password_reset_email.html"
     success_url = reverse_lazy("login")
+
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = "users/password_reset_confirm.html"
