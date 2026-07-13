770ba91a56389b5348ee6e0d204500246f2766d3
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-14 01:46:20 +0600

    remobe and update settings.py

--- event_management/settings.py
+++ event_management/settings.py
@@ -114,7 +114,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.CustomUser"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'event_list'
+LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@eventsystem.com'
