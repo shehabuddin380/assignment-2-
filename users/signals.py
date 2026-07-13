801f79243b7d5851b27299c0e8cce76e91c40b40
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-14 01:40:45 +0600

    fix signals

--- users/signals.py
+++ users/signals.py
@@ -1,10 +1,11 @@
 # users/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User, Group
+from django.conf import settings
+from django.contrib.auth.models import Group
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def assign_group_to_user(sender, instance, created, **kwargs):
     if created and not instance.groups.exists():
-        group = Group.objects.get(name='Participant')
+        group, _ = Group.objects.get_or_create(name='Participant')
         instance.groups.add(group)
