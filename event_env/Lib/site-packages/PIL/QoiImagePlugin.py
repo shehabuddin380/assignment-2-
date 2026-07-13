fe078c9839de430a76e478cf0274eef383874c98
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-14 02:57:57 +0600

    all update and maigretion or mekmaigretion and django updat etc.

--- event_env/Lib/site-packages/PIL/QoiImagePlugin.py
+++ event_env/Lib/site-packages/PIL/QoiImagePlugin.py
@@ -25,6 +25,7 @@ class QoiImageFile(ImageFile.ImageFile):
     format_description = "Quite OK Image"
 
     def _open(self) -> None:
+        assert self.fp is not None
         if not _accept(self.fp.read(4)):
             msg = "not a QOI file"
             raise SyntaxError(msg)
@@ -50,7 +51,7 @@ class QoiDecoder(ImageFile.PyDecoder):
         hash_value = (r * 3 + g * 5 + b * 7 + a * 11) % 64
         self._previously_seen_pixels[hash_value] = value
 
-    def decode(self, buffer: bytes | Image.SupportsArrayInterface) -> tuple[int, int]:
+    def decode(self, buffer: Image.DecoderInput) -> tuple[int, int]:
         assert self.fd is not None
 
         self._previously_seen_pixels = {}
@@ -223,7 +224,7 @@ class QoiEncoder(ImageFile.PyEncoder):
             data += self._write_run()
         data += bytes((0, 0, 0, 0, 0, 0, 0, 1))  # padding
 
-        return len(data), 0, data
+        return len(data), 0, bytes(data)
 
 
 Image.register_open(QoiImageFile.format, QoiImageFile, _accept)
