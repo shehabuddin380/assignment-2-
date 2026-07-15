014eed6a5130e33f81b5932e2bf87a380dcbcb36
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-16 00:11:15 +0600

    instale npm css and fix update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './tasks/templates/**/*.html',
+    './users/templates/**/*.html',
+    './templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
