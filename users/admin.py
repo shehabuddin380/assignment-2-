0e65e1fd696c157ff8d47f7a0669d307eb7b8e2f
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-17 20:59:26 +0600

    user admin updat and add phone number and profile picture

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('phone_number', 'profile_picture')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('phone_number', 'profile_picture')}),
+    )
+    list_display = ('username', 'email', 'phone_number', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
